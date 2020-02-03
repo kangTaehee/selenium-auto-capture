273e05a07af0b71037678c519ef3a0fa516f2e99
kangTaehee <ilovekth@nate.com>
2020-02-03 17:32:27 +0900

    Update test.py
    탭 유념

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ import pandas as pd
 
 domain = 'http://ucms.unpl.co.kr' #사이트 도메인
 
-site_id = 'sitename' #사이트 아이디와 같은 폴더 생성
+site_id = 'test' #사이트 아이디와 같은 폴더 생성
 menu_file = site_id + '.csv'
 
 df = pd.read_csv(os.getcwd() + '\\' + menu_file, encoding="UTF-8", low_memory=False,sep=",")
@@ -23,111 +23,111 @@ height = '1080' #수정안해도 됨.
 
 def fullpage_screenshot(driver, file):
 
-    # print("Starting chrome full page screenshot workaround ...")
+	# print("Starting chrome full page screenshot workaround ...")
 
-    total_width = driver.execute_script("return document.body.offsetWidth")
-    total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
-    viewport_width = driver.execute_script("return document.body.clientWidth")
-    viewport_height = driver.execute_script("return window.innerHeight")
-    # print("Total: ({0}, {1}), Viewport: ({2},{3})".format(total_width, total_height,viewport_width,viewport_height))
-    rectangles = []
+	total_width = driver.execute_script("return document.body.offsetWidth")
+	total_height = driver.execute_script("return document.body.parentNode.scrollHeight")
+	viewport_width = driver.execute_script("return document.body.clientWidth")
+	viewport_height = driver.execute_script("return window.innerHeight")
+	# print("Total: ({0}, {1}), Viewport: ({2},{3})".format(total_width, total_height,viewport_width,viewport_height))
+	rectangles = []
 
-    i = 0
-    while i < total_height:
-        ii = 0
-        top_height = i + viewport_height
+	i = 0
+	while i < total_height:
+		ii = 0
+		top_height = i + viewport_height
 
-        if top_height > total_height:
-            top_height = total_height
+		if top_height > total_height:
+			top_height = total_height
 
-        while ii < total_width:
-            top_width = ii + viewport_width
+		while ii < total_width:
+			top_width = ii + viewport_width
 
-            if top_width > total_width:
-                top_width = total_width
+			if top_width > total_width:
+				top_width = total_width
 
-            # print("Appending rectangle ({0},{1},{2},{3})".format(ii, i, top_width, top_height))
-            rectangles.append((ii, i, top_width,top_height))
+			# print("Appending rectangle ({0},{1},{2},{3})".format(ii, i, top_width, top_height))
+			rectangles.append((ii, i, top_width,top_height))
 
-            ii = ii + viewport_width
+			ii = ii + viewport_width
 
-        i = i + viewport_height
+		i = i + viewport_height
 
-    stitched_image = Image.new('RGB', (total_width, total_height))
-    previous = None
-    part = 0
+	stitched_image = Image.new('RGB', (total_width, total_height))
+	previous = None
+	part = 0
 
-    for rectangle in rectangles:
-        if not previous is None:
-            driver.execute_script("window.scrollTo({0}, {1})".format(rectangle[0], rectangle[1]))
-            # print("Scrolled To ({0},{1})".format(rectangle[0], rectangle[1]))
-            time.sleep(0.2)
+	for rectangle in rectangles:
+		if not previous is None:
+			driver.execute_script("window.scrollTo({0}, {1})".format(rectangle[0], rectangle[1]))
+			# print("Scrolled To ({0},{1})".format(rectangle[0], rectangle[1]))
+			time.sleep(0.2)
 
-        file_name = "part_{0}.png".format(part)
-        # print("Capturing {0} ...".format(file_name))
+		file_name = "part_{0}.png".format(part)
+		# print("Capturing {0} ...".format(file_name))
 
-        driver.get_screenshot_as_file(file_name)
-        screenshot = Image.open(file_name)
+		driver.get_screenshot_as_file(file_name)
+		screenshot = Image.open(file_name)
 
-        if rectangle[1] + viewport_height > total_height:
-            offset = (rectangle[0], total_height - viewport_height)
-        else:
-            offset = (rectangle[0], rectangle[1])
+		if rectangle[1] + viewport_height > total_height:
+			offset = (rectangle[0], total_height - viewport_height)
+		else:
+			offset = (rectangle[0], rectangle[1])
 
-        # print("Adding to stitched image with offset ({0}, {1})".format(offset[0],offset[1]))
-        stitched_image.paste(screenshot, offset)
+		# print("Adding to stitched image with offset ({0}, {1})".format(offset[0],offset[1]))
+		stitched_image.paste(screenshot, offset)
 
-        del screenshot
-        os.remove(file_name)
-        part = part + 1
-        previous = rectangle
+		del screenshot
+		os.remove(file_name)
+		part = part + 1
+		previous = rectangle
 
-    stitched_image.save(file)
-    # print("Finishing chrome full page screenshot workaround...")
-    return True
+	stitched_image.save(file)
+	# print("Finishing chrome full page screenshot workaround...")
+	return True
 
 for width in resolutions:
-		# 크롬설정 성공		
-		# options = webdriver.ChromeOptions()
-		# options.add_argument('headless')
-		# options.add_argument('window-size=' + width + 'x' + height)
-		# options.add_argument("disable-gpu")
-		# driver = webdriver.Chrome(os.getcwd() + '\\driver\\chromedriver.exe', chrome_options=options)
-		
-		# 엣지설정 성공
-		# Edge(MicrosoftWebDriver)를 이용해 Edge를 실행합니다.
-		# driver = webdriver.Edge(executable_path='C:/Users/user/Downloads/edgedriver_win64/msedgedriver.exe')
-		
-		# IE설정 열댓번만에 성공...이런 신발...
-		# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer.exe') 실패..실패..실패..
-		# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer2.4.exe') 성공... 화면 틀어짐 실패
-		# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer3.9.0.exe') 실패
-		# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer4.exe') 실패
-		driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer380.exe') #성공
-    try:
-        os.stat(os.getcwd() + '\\' + site_id + '\\' + str(width) + 'px')
-    except:
-        os.mkdir(os.getcwd() + '\\' + site_id + '\\' + str(width) + 'px')
+	# 크롬설정 성공		
+	# options = webdriver.ChromeOptions()
+	# options.add_argument('headless')
+	# options.add_argument('window-size=' + width + 'x' + height)
+	# options.add_argument("disable-gpu")
+	# driver = webdriver.Chrome(os.getcwd() + '\\driver\\chromedriver.exe', chrome_options=options)
+
+	# 엣지설정 성공
+	# Edge(MicrosoftWebDriver)를 이용해 Edge를 실행합니다.
+	# driver = webdriver.Edge(executable_path='C:/Users/user/Downloads/edgedriver_win64/msedgedriver.exe')
+
+	# IE설정 열댓번만에 성공...이런 신발...
+	# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer.exe') 실패..실패..실패..
+	# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer2.4.exe') 성공... 화면 틀어짐 실패
+	# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer3.9.0.exe') 실패
+	# driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer4.exe') 실패
+	driver = webdriver.Ie(os.getcwd() + '\\driver\\IEDriverServer380.exe') #성공
+	try:
+		os.stat(os.getcwd() + '\\' + site_id + '\\' + str(width) + 'px')
+	except:
+		os.mkdir(os.getcwd() + '\\' + site_id + '\\' + str(width) + 'px')
 
 
-    for idx, row in df.iterrows():
-        try:
-            driver.get(domain + row['ALL_MENU_COURS'])
-        except Exception as e:
-            print(e)
-            continue
-        try:
-            alert = driver.switch_to_alert()
-            alert.accept()
-        except Exception as e:
-            print(e)
-            pass
+	for idx, row in df.iterrows():
+		try:
+			driver.get(domain + row['ALL_MENU_COURS'])
+		except Exception as e:
+			print(e)
+			continue
+		try:
+			alert = driver.switch_to_alert()
+			alert.accept()
+		except Exception as e:
+			print(e)
+			pass
 
-        try:
-            fullpage_screenshot(driver, os.getcwd() + '\\' + site_id + '\\' + str(width) + 'px\\' + str(row['MENU_NO']) + '-' + str(row['MENU_NM']) + '(' + width + 'px).png')
-            print('====', idx, row['MENU_NO'], '======')
-        except Exception as e:
-            print(e)
-            continue
+		try:
+			fullpage_screenshot(driver, os.getcwd() + '\\' + site_id + '\\' + str(width) + 'px\\' + str(row['MENU_NO']) + '-' + str(row['MENU_NM']) + '(' + width + 'px).png')
+			print('====', idx, row['MENU_NO'], '======')
+		except Exception as e:
+			print(e)
+			continue
 
-    driver.quit()
+	driver.quit()
